fc29b327c03db6f0abe7c402e2271878d6950399
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2024-03-14 14:57:42 +0900

    feat: install pillow for imagefield

--- obab_server/settings/base_settings.py
+++ obab_server/settings/base_settings.py
@@ -109,6 +109,8 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = "/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
