dffa11c96b0277a3118e4c1f318bb9d9b0d71e96
beak jong seoung <jjong015189@gmail.com>
2024-03-18 12:29:49 +0900

    feat: Apply Swagger
    - 스웨거 관련 문서 등록

--- obab_server/urls.py
+++ obab_server/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from obab_server.swagger import get_swagger_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('recipes.urls'))
+    path('',include('recipes.urls')),
 ]
+
+urlpatterns += get_swagger_urls()
