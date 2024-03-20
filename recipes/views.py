3898eb825828a8536ade89b8ff2345b4a2b1d466
beak jong seoung <jjong015189@gmail.com>
2024-03-20 13:46:38 +0900

    fix: Add authentication and permission to FoodRecipesListCreateAPIView
    - JWT 토큰을 이용해여 인증, IsAuthenticatedOrReadOnly 권한 설정

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,21 @@
 from rest_framework import generics
 from rest_framework.parsers import MultiPartParser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from .serializer import FoodRecipesSerializer
 from .models import FoodRecipes
+from accounts.functions import get_user_id
 
 class FoodRecipesListCreateAPIView(generics.ListCreateAPIView):
     parser_classes = (MultiPartParser,)
     queryset = FoodRecipes.objects.all()
     serializer_class = FoodRecipesSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=get_user_id(self.request))
 
 class FoodRecipesRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = FoodRecipes.objects.all()
