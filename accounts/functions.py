5846a3f73f9964158ab2b7b48b2906ac484b41f4
beak jong seoung <jjong015189@gmail.com>
2024-03-20 13:44:32 +0900

    feat: Create get_user_id function
    - AccessToken을 가지고 User Instance를 가지고오는 함수 추가

---
+++ accounts/functions.py
@@ -0,0 +1,10 @@
+from rest_framework_simplejwt.tokens import AccessToken
+from accounts.models import User
+
+def get_user_id(request):
+    auth_header = request.headers.get('Authorization')
+    access_tokne = auth_header.split(' ')[1]
+    decoded = AccessToken(access_tokne)
+    user_id = decoded['user_id']
+    user_instance = User.objects.get(id=user_id)
+    return user_instance
