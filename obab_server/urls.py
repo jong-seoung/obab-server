8c60553a0e8060f3e4afbb11fd5cd8214e720a97
beak jong seoung <jjong015189@gmail.com>
2024-03-21 10:59:20 +0900

    chore: Add forgotten files to previous commit
    - 이전 커밋에 까먹은 파일 추가

--- obab_server/urls.py
+++ obab_server/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('recipes.urls')),
     path('accounts/',include('accounts.urls')),
+    path('comments/', include('comments.urls'))
 ]
 
 urlpatterns += get_swagger_urls()
