f64076574e9fe7f9a14eca2bfdb07da318bd8750
beak jong seoung <jjong015189@gmail.com>
2024-04-08 11:34:21 +0900

    refactor: divide Viewset for add description about comment
    - 댓글 관련 api swagger 추가를 위한 commentViewset 분리
    - 댓글 관련 api swagger 설명 추가

--- comments/viewsets.py
+++ comments/viewsets.py
@@ -1,6 +1,10 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework import viewsets
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, DestroyModelMixin
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
 
 from recipes.permissions import IsOwnerOrReadOnly
 from .serializers import CommentSerializer
@@ -9,7 +13,7 @@ from accounts.functions import get_user_id
 
 from .models import Comments
 
-class CommentViewSet(viewsets.ModelViewSet):
+class CommentViewSet(CreateModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Comments.objects.all()
@@ -17,3 +21,33 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=get_user_id(self.request))
+
+    @swagger_auto_schema(
+            operation_id="댓글 생성",
+            operation_description='root는 대댓글을 달 부모 댓글id 입력\n'
+            '일반 댓글의 경우 root에는 null값 전송',
+            )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+            operation_id="댓글 전체 수정",
+            operation_description='recipe는 본문의 id값\n'
+            '모든 파라미터를 입력해서 보내주셔야합니다.',
+            )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+            operation_id='댓글 부분 수정',
+            operation_description='recipe는 본문의 id값\n'
+            '수정할 내용만 파라미터로 보내주시면 됩니다.',
+            )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+            operation_id='댓글 삭제',
+            operation_description='댓글 삭제',)
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
