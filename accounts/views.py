444959acae9e73576ea1ec54976d562afcc7f2d6
beak jong seoung <jjong015189@gmail.com>
2024-04-11 13:04:08 +0900

    style: replace import order
    - import문 순서 재 배치

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,24 @@
-from django.conf import settings
-# total
 import requests
-from django.shortcuts import redirect
+
+from django.conf import settings
 from django.http import JsonResponse
-from rest_framework.views import APIView
+from django.shortcuts import redirect
+
+from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from allauth.socialaccount.models import SocialAccount
-from dj_rest_auth.registration.views import SocialLoginView
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from dj_rest_auth.registration.views import SocialLoginView
+
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
 from accounts.models import User
 from accounts.tokens import TokenResponseSerializer
-from drf_yasg import openapi
 
 
 class Constants:
