3ffbc01379f6921cec0695eea155603d6c63703c
beak jong seoung <jjong015189@gmail.com>
2024-04-20 18:19:32 +0900

    refactor: change userinfo url, add userinfo swagger
    - 유저 정보 url 경로 변경
    - 유저 정보 스웨거 작성

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 from users.views import UserInfoViews
 urlpatterns = [
-    path('userinfo/', UserInfoViews.as_view(), name='userinfo'),
+    path('', UserInfoViews.as_view(), name='userinfo'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
+from drf_yasg.utils import swagger_auto_schema
+
 from django.core.files.storage import default_storage
 
 from rest_framework.parsers import MultiPartParser
-from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -17,6 +19,22 @@ class UserInfoViews(RetrieveUpdateAPIView):
         user = get_user_id(self.request)
         return user
 
+    @swagger_auto_schema(tags=['유저 정보'])
+    def get(self, request, *args, **kwargs):
+        """
+        유저 정보 조회
+        ---
+        """
+        return super().get(request, *args, **kwargs)
+    
+    @swagger_auto_schema(tags=['유저 정보'])
+    def patch(self, request, *args, **kwargs):
+        """
+        유저 정보 부분 수정
+        ---
+        """
+        return super().patch(request, *args, **kwargs)
+    
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.serializer_class(instance)
